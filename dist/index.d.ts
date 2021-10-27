dd9dd319d1281b9a43bc906ce6fa7d3e04e4a973
Diego <diegopm2000@gmail.com>
2021-10-27 21:47:27 +0200

    fix: removed dist & added prepare script

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export default class Calculator {
-    static add(a: number, b: number): number;
-    static substract(a: number, b: number): number;
-    static multiply(a: number, b: number): number;
-    static divide(a: number, b: number): number;
-}
